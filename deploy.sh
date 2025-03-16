0d653c400e1ba8d6eea2c2a1de8d2111a65baf07
ZotyaNET <49711103+ZotyaNET@users.noreply.github.com>
2025-03-16 14:15:34 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@ if [ -z "$1" ]
     exit 1
 fi
 export deploymentname=$1
-gcloud deployment-manager deployments create  $deploymentname --config main.teaorinfo.yaml
+gcloud deployment-manager deployments create  $deploymentname --config main.vpn.yaml
 echo "$deploymentname-vm was deployed."
 sleep 5
 export staticip="`gcloud compute instances describe $deploymentname-vm | grep -Po 'natIP: \K(.*)'`"
 if [[ ! -z "$staticip" ]]; then
-    echo 'Congratulations! teaor.info deployment is completed.'
+    echo 'Congratulations! VPN deployment is completed.'
     echo 'Now, do DNS mapping with static IP:' $staticip
     echo 'then, run change-domain.sh via ssh'
 fi
