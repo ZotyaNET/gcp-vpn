d07ab577061e3c8a4073c5dca5e93ad6ba276029
ZotyaNET <49711103+ZotyaNET@users.noreply.github.com>
2025-04-08 16:56:13 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,21 +8,12 @@ export deploymentname=$1
 
 # Get the first PROJECT_ID from the list
 PROJECT_ID=$(gcloud projects list --format="value(PROJECT_ID)" | head -n 1)
-
-echo "First PROJECT_ID found: $PROJECT_ID"
-read -p "Do you want to use this project? (Y/n): " CONFIRM
-
-if [[ "$CONFIRM" =~ ^[Yy]?$ ]]; then
-  echo "Setting project to $PROJECT_ID..."
-  gcloud config set project "$PROJECT_ID"
-else
-  echo "Aborted. Project not set."
-fi
-
+echo "Setting project to $PROJECT_ID..."
+gcloud config set project "$PROJECT_ID"
 gcloud deployment-manager deployments create  $deploymentname --config main.vpn.yaml
-echo "$deploymentname-vm was deployed."
+echo "$deploymentname was deployed."
 sleep 5
-export staticip="`gcloud compute instances describe $deploymentname-vm | grep -Po 'natIP: \K(.*)'`"
+export staticip="`gcloud compute instances describe $deploymentname | grep -Po 'natIP: \K(.*)'`"
 if [[ ! -z "$staticip" ]]; then
     echo 'Congratulations! VPN deployment is completed.'
     echo 'Now, do DNS mapping with static IP:' $staticip
